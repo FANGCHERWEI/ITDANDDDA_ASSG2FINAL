0010f741bc7f5f80db0b11d3855b22cadbab0b91
rebeccalim20 <rebeccalim20@users.noreply.github.com>
2021-12-26 04:39:04 +0800

    added the layout of the ui to the scene ui_game scene

---
+++ Assets/UI_Files/Scripts/SideTabGrp.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideTabGrp : MonoBehaviour
+{
+    //create a list of gameobject to store for the list of side tab button
+    public List<TabButton> tabButtons;
+    //the button is being selected
+    public TabButton selectedTab;
+    //list of gameobject going to swap 
+    public List<GameObject> objectToswap;
+
+    //this method could take in type of button
+
+    private void Start()
+    {
+        
+    }
+   
+
+    public void OnTabSelected(TabButton btn)
+    {
+        Debug.Log("selectbtnpls");
+        selectedTab = btn;
+        //get sibling index from the transform btn
+        
+        int index = btn.transform.GetSiblingIndex();
+        print(index);
+        //usage of for loop to loop inside of the object gameobject panel
+        for (int i = 0; i < objectToswap.Count; i++)
+        {
+            for (int j = 0; j < tabButtons.Count; j++)
+            {
+                //check if the index is eqal to the particular 
+                if (i == index && tabButtons[i])
+                {
+                    objectToswap[i].SetActive(true);
+                }
+                else
+                {
+                    objectToswap[i].SetActive(false);
+                }
+            }
+
+        }
+    }
+
+}
